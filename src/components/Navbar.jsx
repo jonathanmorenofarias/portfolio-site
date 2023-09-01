f4e57f01861d85eb04cb0b05d067662e3bdf2dd6
Jonathan Moreno <jonathan.moreno.farias02@gmail.com>
2023-09-01 13:41:39 -0700

    Testing netlify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from "react"
-import {RxHamburgerMenu} from "react-icons/rx"
-import {VscChromeClose} from "react-icons/vsc"
+
 
 const Navbar = () => {
     return (
       <header className="shadow-md">
         <h1>Jonathan Moreno</h1>
         <nav className="">
-            <a>About me</a>
-            <a>Skills</a>
-            <a>Projects</a>
-            <a>Contact</a>
+            <a href="https://www.youtube.com">About me</a>
+            <a href="https://www.youtube.com">Skills</a>
+            <a href="https://www.youtube.com">Projects</a>
+            <a href="https://www.youtube.com">Contact</a>
         </nav>
 
       </header>
